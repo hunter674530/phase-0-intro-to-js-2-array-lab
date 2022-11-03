1ecd3f846b2e91cd05228b3279fd60f13e43b48a
Hunter Wood <hunter674530@gmail.com>
2022-11-03 08:40:04 -0600

    commit tree 24520e695a423c86967667db0459de84b263998f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  return cats.push(name);
+}
+destructivelyAppendCat("Simon");
+
+function destructivelyPrependCat(name){
+  return cats.unshift(name);
+}
+destructivelyPrependCat("Annie");
+
+function destructivelyRemoveLastCat(name){
+  return cats.pop();
+}
+destructivelyRemoveLastCat();
+       
+function destructivelyRemoveFirstCat(name){
+  return cats.shift();
+}
+destructivelyRemoveFirstCat();
+        
+     
+function appendCat(name){
+  const cats1 = [...cats, name]
+  return cats1
+}
+
+function prependCat(name){
+  const cats2 = [name, ...cats]
+  return cats2
+}
+
+function removeLastCat(){
+  const cats3 = cats.slice(0,-1);
+  return cats3;
+}
+
+function removeFirstCat(){
+  const cats4 = cats.slice(1);
+  return cats4;
+}
